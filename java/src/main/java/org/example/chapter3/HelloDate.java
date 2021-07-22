94f780fae39aef493a8adb5a8fb9fd10a8a5cca1
yuhh <yuhaihan2014@outlook.com>
2021-07-22 23:13:02 +0800

    think in java chapter3 practise

---
+++ java/src/main/java/org/example/chapter3/HelloDate.java
@@ -0,0 +1,10 @@
+package org.example.chapter3;
+
+import java.util.Date;
+
+public class HelloDate {
+    public static void main(String[] args) {
+        System.out.println("Hello, it's: ");
+        System.out.println(new Date());
+    }
+}
